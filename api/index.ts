c20d49f4736a0705717d0e0536ea8a7b5804b5f1
Vlad K <wlad3430@gmail.com>
2024-05-30 00:19:54 +0300

    upd

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,47 @@
-import app from '../src/app'
+import express from 'express'
+import cors from 'cors'
 import { Config } from '../src/config'
+import { ErrorHandler } from '../src/middlewares'
 import { Logger } from '../src/utils'
+import { EmailRoutes, TelegramRoutes } from '../src/routes'
 
-const PORT = Config.getPort()
+class App {
+  public app: express.Application
 
-app.listen(PORT, () => {
-  Logger.info(`Server is running on port ${PORT}`)
-})
+  constructor() {
+    this.app = express()
+    this.config()
+    this.routes()
+    this.errorHandling()
+    this.logInfo()
+  }
+
+  private config(): void {
+    Config.loadConfig()
+    this.app.use(cors())
+    this.app.use(express.json())
+
+    const PORT = Config.getPort()
+    this.app.listen(PORT, () => {
+      Logger.info(`Server is running on port ${PORT}`)
+    })
+  }
+
+  private routes(): void {
+    this.app.use('/api/telegram', TelegramRoutes)
+    this.app.use('/api/email', EmailRoutes)
+    this.app.get('/', (req, res) => {
+      res.send('Server connected')
+    })
+  }
+
+  private errorHandling(): void {
+    this.app.use(ErrorHandler.handle)
+  }
+
+  private logInfo(): void {
+    Logger.info('Server started')
+  }
+}
+
+export default new App().app
